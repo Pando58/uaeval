95731e0dd0e2d75e8386d646a0cb0983bd9ebe93
Enrique Pando <enr.pando@gmail.com>
2021-04-27 10:00:49 -0500

    Reciclar css en vistas para agregar del panel de administracion

--- src/components/vistasAdmin/Alumnos/AlumnosAgregar.vue
+++ src/components/vistasAdmin/Alumnos/AlumnosAgregar.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="d-flex flex-col">
+  <div class="d-flex flex-col tarjeta-form">
     <div class="cabeza">
       <i class="fas fa-arrow-left pointer" style="color: #AAA" @click="$emit('irTablaAlumnos')"></i>
     </div>
     <div class="contenido">
       <div class="text-center">
         <h4>Agregar alumno</h4>
+        <span class="msg-error" v-if="error">{{ error }}</span>
       </div>
       <div class="nombre">
-        <input type="text" placeholder="Nombre(s)">
-        <input type="text" placeholder="Apellido Paterno">
-        <input type="text" placeholder="Apellido Materno">
+        <input type="text" placeholder="Nombre(s)" v-model="nombre">
+        <input type="text" placeholder="Apellido Paterno" v-model="apellido_p">
+        <input type="text" placeholder="Apellido Materno" v-model="apellido_m">
       </div>
       <div class="d-flex align-center">
-        <input type="text" class="flex-grow-2" placeholder="Matrícula">
+        <input type="text" class="flex-grow-2" placeholder="Matrícula" v-model="usuario">
         <span class="etiqueta">Grupo:</span>
-        <select class="flex-grow-1">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="opel">Opel</option>
-          <option value="audi">Audi</option>
+        <select class="flex-grow-1" v-model="grupo">
+          <option disabled value="">----</option>
+          <option value="1">Volvo</option>
+          <option value="2">Saab</option>
+          <option value="3">Opel</option>
+          <option value="4">Audi</option>
         </select>
         <!-- <input type="text" class="flex-grow-1" placeholder="Grupo"> -->
       </div>
       <div class="f-right">
-        <button class="material-btn verde">Agregar</button>
+        <button class="material-btn verde" @click="enviar">Agregar</button>
       </div>
     </div>
   </div>
@@ -33,55 +35,27 @@
 <script>
 
 export default {
-  name: 'AlumnosAgregar'
+  name: 'AlumnosAgregar',
+  data: () => ({
+    nombre: '',
+    apellido_p: '',
+    apellido_m: '',
+    usuario: '',
+    grupo: '',
+    error: null
+  }),
+  methods: {
+    enviar: function() {}
+  }
 }
 
 </script>
 
 <style scoped>
 
-.contenido {
-  font-family: Roboto, sans-serif;
-}
-
 .nombre {
   display: grid;
   grid-template-columns: 2fr 1fr 1fr;
-}
-
-input,
-select {
-  margin: 6px;
-  min-width: 0;
-  padding: 6px;
-  font-size: 0.9rem;
-  border: 1px solid #CCC;
-  border-radius: 4px;
-  outline: none;
-  transition: border-color 0.1s;
-}
-
-input:focus,
-select:focus {
-  border-color: rgb(43, 189, 74);
-}
-
-.etiqueta {
-  margin-left: 8px;
-  font-size: 0.9rem;
-  color: #777;
-}
-
-button {
-  font-family: Roboto, sans-serif;
-  font-weight: 500;
-  font-size: 0.9rem;
-  margin: 10px 6px 4px 0;
-}
-
-h4 {
-  margin: 0 0 12px 0;
-  color: #444;
 }
 
 </style>
--- src/components/vistasAdmin/Docentes/DocentesAgregar.vue
+++ src/components/vistasAdmin/Docentes/DocentesAgregar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-col">
+  <div class="d-flex flex-col tarjeta-form">
     <div class="cabeza">
       <i class="fas fa-arrow-left pointer" style="color: #AAA" @click="$emit('irTabla')"></i>
     </div>
@@ -34,27 +34,7 @@ export default {
     error: null
   }),
   methods: {
-    enviar: function() {
-      /* api.post('/administradores.php', {
-        accion: 'agregar',
-        datos: {
-          nombre: this.nombre,
-          apellido_p: this.apellido_p,
-          apellido_m: this.apellido_m
-        }
-      }).then(res => {
-        const data = res.data;
-        console.log(res);
-
-        if (data.estado != 'ok') {
-          if (data.err_id == 1) {
-            this.error = 'Completa los campos requeridos';
-          }
-        } else {
-          this.error = null;
-        }
-      }).catch(err => console.log(err)); */
-    }
+    enviar: function() {}
   }
 }
 
@@ -62,63 +42,10 @@ export default {
 
 <style scoped>
 
-.contenido {
-  font-family: Roboto, sans-serif;
-}
-
 .nombre {
   display: grid;
   grid-template-columns: 2fr 1fr 1fr;
   margin-top: 8px;
-}
-
-input,
-select {
-  margin: 6px;
-  min-width: 0;
-  padding: 6px;
-  font-size: 0.9rem;
-  border: 1px solid #CCC;
-  border-radius: 4px;
-  outline: none;
-  transition: border-color 0.1s;
-}
-
-input:focus,
-select:focus {
-  border-color: rgb(43, 189, 74);
-}¨
-
-.etiqueta {
-  margin-left: 8px;
-  font-size: 0.9rem;
-  color: #777;
-}
-
-button {
-  font-family: Roboto, sans-serif;
-  font-weight: 500;
-  font-size: 0.9rem;
-  margin: 10px 6px 4px 0;
-}
-
-h4 {
-  margin: 0 0 12px 0;
-  color: #444;
-}
-
-.msg-error {
-  color: #F44;
-}
-
-.permisos {
-  padding: 6px;
-}
-
-.permisos label {
-  /* border: 1px solid #CCC; */
-  margin: 6px;
-  padding: 4px;
 }
 
 </style>
