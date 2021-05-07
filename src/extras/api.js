d1f78cef3aaf6e8e88c71e24eff6e3144fd71b9c
Enrique Pando <enr.pando@gmail.com>
2021-05-07 09:04:50 -0500

    Agregar archivo de configuracion para tomar la URL de la API

--- src/extras/api.js
+++ src/extras/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import config from '../../api_config.json';
 
 export default axios.create({
-  baseURL: 'http://localhost/uaeval_api',
+  baseURL: (config || {}).url || 'http://localhost/uaeval_api',
   headers: {
     'Content-type': 'application/json'
   }
