7a71a3d3cdacb375b4fe0b786fe42e6b0eeb9c8a
Enrique Pando <enr.pando@gmail.com>
2021-05-07 13:53:32 -0500

    Arreglar cache

--- src/extras/api.js
+++ src/extras/api.js
@@ -4,6 +4,8 @@ import config from '../../api_config.json';
 export default axios.create({
   baseURL: (config || {}).url || 'http://localhost/uaeval_api',
   headers: {
-    'Content-type': 'application/json'
+    'Content-type': 'application/json',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache'
   }
 });
