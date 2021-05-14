5af1f13c89d334c54200076818001abc5cfe06b0
Enrique Pando <enr.pando@gmail.com>
2021-05-14 14:09:38 -0500

    Opcion de copiar tabla en estadisticas y pegar al centro la leyenda

--- src/components/vistasAdmin/AdminEstadisticas.vue
+++ src/components/vistasAdmin/AdminEstadisticas.vue
@@ -2,6 +2,12 @@
   <div id="admin-estadisticas">
     <h3 class="titulo-graficas">Resultados por categoría</h3>
     <div class="graficas">
+      <div class="botones">
+        <button class="material-btn verde sombra ver-tabla" @click="copiarTablaGrafica">
+          Copiar tabla
+          <i class="far fa-file-excel"></i>
+        </button>
+      </div>
       <grafica-pastel 
         :categoriaSeleccionada="categoriaSeleccionada"
         :categorias="categorias"
@@ -128,6 +134,80 @@ export default {
           backgroundColor: this.colores
         }]
       };
+    },
+    copiarTablaGrafica() {
+      console.log(this.categorias);
+      console.log(this.datosCategorias);
+
+      
+      const _table = document.createElement('table');
+      const _thead = document.createElement('thead');
+      const _tbody = document.createElement('tbody');
+
+      // Cabecera   | Muy en desacuerdo | En desacuerdo | ...
+      const _tr = document.createElement('tr');
+      _thead.appendChild(_tr);
+
+      this.etiquetas.forEach(i => {
+        const _th = document.createElement('th');
+        _th.innerHTML = i;
+        _th.style.background = '#33a14b';
+        _th.style.border = '1px solid #000';
+        _th.style.color = '#FFF';
+        _tr.appendChild(_th);
+      });
+
+
+      this.categorias.forEach(i => {
+        // Agregar categoria
+        const _tr = document.createElement('tr');
+        const _th = document.createElement('th');
+        _th.innerHTML = i.categoria;
+        _th.style.fontWeight = '400';
+        _th.style.background = '#d6ffdf';
+        _th.style.border = '1px solid #000';
+        _th.colSpan = 5;
+        _tr.appendChild(_th);
+        _tbody.appendChild(_tr);
+
+        // Agregar datos
+        const _tr2 = document.createElement('tr');
+        Object.keys(this.datosCategorias[i.id]).forEach(j => {
+          const _td = document.createElement('td');
+          _td.innerHTML = this.datosCategorias[i.id][j];
+          _td.style.background = '#e8ffed';
+          _td.style.border = '1px solid #000';
+          _tr2.appendChild(_td);
+          _tbody.appendChild(_tr2);
+        });
+      });
+      
+      
+      
+      /* _table.style.position = 'fixed';
+      _table.style.zIndex = '100';
+      _table.style.top = '12px';
+      _table.style.left = '12px';
+      _table.style.background = '#FFF'; */
+
+      _table.style.fontFamily = 'Arial';
+      _table.style.fontSize = '1rem';
+      _table.style.border = '1px solid #000';
+      
+      _table.appendChild(_thead);
+      _table.appendChild(_tbody);
+      document.body.appendChild(_table);
+
+
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.selectNodeContents(_table);
+      selection.removeAllRanges();
+      selection.addRange(range);
+
+      
+      document.execCommand('copy');
+      document.body.removeChild(_table);
     }
   }
 }
@@ -155,6 +235,7 @@ export default {
 }
 
 .leyenda {
+  height: 100%;
   font-family: Roboto, sans-serif;
   font-size: 1rem;
   padding: 1em;
@@ -175,6 +256,28 @@ export default {
 
 .leyenda b {
   font-weight: 500;
+}
+
+.botones {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+  padding: 12px;
+  width: 100%;
+  height: 100%;
+}
+
+.ver-tabla {
+  font-size: 0.9rem;
+  font-family: Poppins, sans-serif;
+}
+
+.ver-tabla i {
+  transform: translateY(10%);
+  font-size: 1.05rem;
+  color: #FFF;
+  margin-left: 0.2rem;
 }
 
 </style>
